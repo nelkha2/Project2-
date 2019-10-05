699748ac02c33208e298686b3f2bb71e82f97a28
Michael Harvanek <mharvanek@gmail.com>
2019-10-05 10:16:01 -0500

    updated queries

--- data/queries.sql
+++ data/queries.sql
@@ -1,17 +1,17 @@
-delete from billboardhot100
+delete from billboardhot100withlyrics
 where rankid >= 304 and rankid < 501
 ;
-select rankid, rank, song_x, artist_x, year, song_y, artist_y, lyrics, geniuslyrics
-from billboardhot100
+select rankid, rank, song, artist, year, geniuslyrics
+from billboardhot100withlyrics
 order by rankid
 ;
 select count(distinct(artist_y))
-from billboardhot100
+from billboardhot100withlyrics
 ;
-select artist_x, count(distinct song_x)
-from billboardhot100
-group by artist_x
-order by count(distinct song_x) desc
+select artist, count(distinct song)
+from billboardhot100withlyrics
+group by artist
+order by count(distinct song) desc
 ;
 select artist, count(*)
 from billboardhot100withlyrics
@@ -19,16 +19,16 @@ group by artist
 order by count(*) desc
 ;
 select *
-from billboardhot100
+from billboardhot100withlyrics
 where geniuslyrics is NULL
 order by rankid
 ;
 select count(*)
-from billboardhot100
-where artist_x like '%featuring%';
+from billboardhot100withlyrics
+where artist like '%featuring%';
 ;
-update billboardhot100
-SET song_x = REPLACE(song_x,'"', '')
+update billboardhot100withlyrics
+SET song = REPLACE(song,'"', '')
 where rankid >= 301 and rankid < 400
 
 --------Ideas---------
@@ -40,18 +40,18 @@ where rankid >= 301 and rankid < 400
 --Things to fix--
 --gangsta's paradise
 
-select rankid, rank, artist_x, song_x, year, geniuslyrics
-from billboardhot100
+select rankid, rank, artist, song, year, geniuslyrics
+from billboardhot100withlyrics
 --where lower(geniuslyrics) like '%instrumental%'
 order by rankid
 ;
-update billboardhot100
+update billboardhot100withlyrics
 set rankid = rankid - 9500
 ;
 select *
-from billboardhot100
+from billboardhot100withlyrics
 where rankid = 362
 ;
-update billboardhot100
+update billboardhot100withlyrics
 set geniuslyrics = '[Instrumental]'
 where lower(geniuslyrics) = 'instrumental'
